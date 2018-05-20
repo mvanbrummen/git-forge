2f94dea80ae4c56cd832d39fc74302b4d7590768
Michael Van Brummen <michaelvanbrummen@gmail.com>
2018-05-20 14:13:47 +1000

    Added click handler for buttons

--- frontend/src/components/repository/CreateButton.js
+++ frontend/src/components/repository/CreateButton.js
@@ -1,30 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-const CreateButton = () => (
-    <div className="level-item">
+class CreateButton extends Component {
 
-        <div class="dropdown">
-            <div class="dropdown-trigger">
-                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+    state = {
+        isActive: false
+    }
 
-                    <span class="icon is-small">
-                        <i class="fa fa-plus" aria-hidden="true"></i>
+    componentDidMount() {
+        document.addEventListener('click', this.handleClickOutside, true);
+    }
 
-                    </span>
-                    <span class="icon is-small">
-                        <i class="fa fa-angle-down" aria-hidden="true"></i>
-                    </span>
-                </button>
-            </div>
-            <div class="dropdown-menu" id="dropdown-menu" role="menu">
-                <div class="dropdown-content">
-                    <a href="#" class="dropdown-item">
-                        Dropdown item
-</a>
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClickOutside, true);
+    }
+
+    handleClickOutside = (e) => {
+        const domNode = ReactDOM.findDOMNode(this);
+
+        if (!domNode || !domNode.contains(e.target)) {
+            this.setState({
+                isActive: false
+            });
+        }
+    }
+
+    handleClick = (e) => {
+        e.preventDefault();
+        this.setState({
+            isActive: !this.state.isActive
+        });
+    }
+
+    render() {
+        return (
+            <div className="level-item">
+
+                <div className={this.state.isActive ? "is-active dropdown" : "dropdown"}>
+                    <div class="dropdown-trigger">
+                        <button class="button"
+                            aria-haspopup="true"
+                            aria-controls="dropdown-menu"
+                            onClick={this.handleClick}>
+
+                            <span class="icon is-small">
+                                <i class="fa fa-plus" aria-hidden="true"></i>
+
+                            </span>
+                            <span class="icon is-small">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                            </span>
+                        </button>
+                    </div>
+                    <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                        <div class="dropdown-content">
+                            <a href="#" class="dropdown-item">New Branch</a>
+                        </div>
+                        <div class="dropdown-content">
+                            <a href="#" class="dropdown-item">New Tag</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-)
+        )
+    }
 
+}
 export default CreateButton;
--- frontend/src/components/repository/DownloadButton.js
+++ frontend/src/components/repository/DownloadButton.js
@@ -1,31 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-const DownloadButton = (props) => (
-    <div className="level-item">
+class DownloadButton extends Component {
 
-        <div class="dropdown">
-            <div class="dropdown-trigger">
-                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+    state = {
+        isActive: false
+    }
 
-                    <span class="icon is-small">
-                        <i class="fa fa-cloud-download"></i>
+    componentDidMount() {
+        document.addEventListener('click', this.handleClickOutside, true);
+    }
 
-                    </span>
-                    <span class="icon is-small">
-                        <i class="fa fa-angle-down" aria-hidden="true"></i>
-                    </span>
-                </button>
-            </div>
-            <div class="dropdown-menu" id="dropdown-menu" role="menu">
-                <div class="dropdown-content">
-                    <a href="#" class="dropdown-item">Download .zip</a>
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClickOutside, true);
+    }
+
+    handleClickOutside = (e) => {
+        const domNode = ReactDOM.findDOMNode(this);
+
+        if (!domNode || !domNode.contains(e.target)) {
+            this.setState({
+                isActive: false
+            });
+        }
+    }
+
+    handleClick = (e) => {
+        e.preventDefault();
+        this.setState({
+            isActive: !this.state.isActive
+        });
+    }
+
+    render() {
+        return (
+            <div className="level-item">
+
+                <div className={this.state.isActive ? "is-active dropdown" : "dropdown"}>
+                    <div class="dropdown-trigger">
+                        <button class="button"
+                            aria-haspopup="true"
+                            aria-controls="dropdown-menu"
+                            onClick={this.handleClick}>
+
+                            <span class="icon is-small">
+                                <i class="fa fa-cloud-download"></i>
+
+                            </span>
+                            <span class="icon is-small">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                            </span>
+                        </button>
+                    </div>
+                    <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                        <div class="dropdown-content">
+                            <a href="#" class="dropdown-item">Download .zip</a>
+                        </div>
+                        <div class="dropdown-content">
+                            <a href="#" class="dropdown-item">Download .tar.gz</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="dropdown-content">
-                    <a href="#" class="dropdown-item">Download .tar.gz</a>
-                </div>
             </div>
-        </div>
-    </div>
-)
+        )
+    }
+}
 
 export default DownloadButton;
